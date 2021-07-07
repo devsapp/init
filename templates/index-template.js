d0fa452ac3b39e1259fee5e16460fb3438674b48
xsahxl <xsahxl@126.com>
2021-07-07 19:31:03 +0800

    feat: 🎸 sandbox 传递参数projectName

--- templates/index-template.js
+++ templates/index-template.js
@@ -1,10 +1,12 @@
 const { dk } = require('@serverless-devs/dk');
 
-const handler = dk((ctx) => {
+const baseHandler = (ctx) => {
   // ctx 包含 ctx.req 可获取入参信息
   return {
-    json: { title: 'hello serverless devs' },
+    json: { title: 'hello serverless dk' },
   };
-});
+};
+
+const handler = dk(baseHandler);
 
 exports.handler = handler;
